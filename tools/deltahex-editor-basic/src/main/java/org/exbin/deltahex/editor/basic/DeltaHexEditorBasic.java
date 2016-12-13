5db8a43d48bac6521d6fb1d6c9b7b43cf79d85cc
hajdam <hajdam@users.sf.net>
2016-12-13 18:55:32 +0100

    Added encoding selection dialog

--- tools/deltahex-editor-basic/src/main/java/org/exbin/deltahex/editor/basic/DeltaHexEditorBasic.java
+++ tools/deltahex-editor-basic/src/main/java/org/exbin/deltahex/editor/basic/DeltaHexEditorBasic.java
@@ -16,7 +16,12 @@
 package org.exbin.deltahex.editor.basic;
 
 import java.awt.BorderLayout;
+import java.awt.Toolkit;
+import java.awt.datatransfer.Clipboard;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
@@ -24,12 +29,20 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.AbstractAction;
 import javax.swing.Action;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
+import org.exbin.deltahex.CaretMovedListener;
+import org.exbin.deltahex.CaretPosition;
+import org.exbin.deltahex.EditationMode;
+import org.exbin.deltahex.EditationModeChangedListener;
+import org.exbin.deltahex.Section;
+import org.exbin.deltahex.SelectionChangedListener;
+import org.exbin.deltahex.SelectionRange;
 import org.exbin.deltahex.operation.swing.CodeAreaUndoHandler;
 import org.exbin.deltahex.operation.swing.CodeCommandHandler;
 import org.exbin.deltahex.swing.CodeArea;
@@ -250,12 +263,49 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
                 updateUndoState();
                 codeArea.repaint();
             }
-            
-            private void updateUndoState() {
-                undoEditAction.setEnabled(undoHandler.canUndo());
-                redoEditAction.setEnabled(undoHandler.canRedo());
+        });
+        codeArea.addEditationModeChangedListener(new EditationModeChangedListener() {
+            @Override
+            public void editationModeChanged(EditationMode editationMode) {
+                switch (editationMode) {
+                    case INSERT: {
+                        editationModeLabel.setText("INS");
+                        break;
+                    }
+                    case OVERWRITE: {
+                        editationModeLabel.setText("OVR");
+                        break;
+                    }
+                    default: {
+                        throw new IllegalStateException("Unexpected editation mode " + editationMode.name());
+                    }
+                }
             }
         });
+        codeArea.addCaretMovedListener(new CaretMovedListener() {
+            @Override
+            public void caretMoved(CaretPosition caretPosition, Section section) {
+                if (caretPosition != null) {
+                    positionLabel.setText(caretPosition.getDataPosition() + ":" + caretPosition.getCodeOffset());
+                }
+            }
+        });
+        codeArea.addSelectionChangedListener(new SelectionChangedListener() {
+            @Override
+            public void selectionChanged(SelectionRange selection) {
+                updateClipboardState();
+            }
+        });
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        clipboard.addFlavorListener(new FlavorListener() {
+            @Override
+            public void flavorsChanged(FlavorEvent e) {
+                updateClipboardState();
+            }
+        });
+        updateUndoState();
+        updateClipboardState();
+        openFileButton.setText("Open");
     }
 
     /**
@@ -291,7 +341,9 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         statusBarPanel = new javax.swing.JPanel();
         statusBarSeparator = new javax.swing.JSeparator();
         textStatusPanel = new javax.swing.JPanel();
-        jLabel1 = new javax.swing.JLabel();
+        editationModeLabel = new javax.swing.JLabel();
+        positionLabel = new javax.swing.JLabel();
+        encodingLabel = new javax.swing.JLabel();
         mainMenuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         newFileMenuItem = new javax.swing.JMenuItem();
@@ -309,6 +361,8 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         editPasteMenuItem = new javax.swing.JMenuItem();
         editDeleteMenuItem = new javax.swing.JMenuItem();
         selectAllMenuItem = new javax.swing.JMenuItem();
+        aboutMenu = new javax.swing.JMenu();
+        aboutMenuItem = new javax.swing.JMenuItem();
 
         editUndoPopupMenuItem.setAction(undoEditAction);
         editUndoPopupMenuItem.setText("Undo");
@@ -327,7 +381,7 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         editCopyPopupMenuItem.setText("Copy");
         mainPopupMenu.add(editCopyPopupMenuItem);
 
-        editPastePopupMenuItem.setAction(copyEditAction);
+        editPastePopupMenuItem.setAction(pasteEditAction);
         editPastePopupMenuItem.setText("Paste");
         mainPopupMenu.add(editPastePopupMenuItem);
 
@@ -416,19 +470,48 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         statusBarSeparator.setAutoscrolls(true);
         statusBarPanel.add(statusBarSeparator, java.awt.BorderLayout.PAGE_START);
 
-        jLabel1.setText("jLabel1");
+        editationModeLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        editationModeLabel.setText("OVR");
+        editationModeLabel.setToolTipText("Current editation mode");
+        editationModeLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        editationModeLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                editationModeLabelMouseClicked(evt);
+            }
+        });
+
+        positionLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        positionLabel.setText("0:0");
+        positionLabel.setToolTipText("Current cursor position");
+        positionLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+
+        encodingLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        encodingLabel.setText("UTF-8");
+        encodingLabel.setToolTipText("Currently active encoding");
+        encodingLabel.setBorder(javax.swing.BorderFactory.createEtchedBorder());
+        encodingLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                encodingLabelMouseClicked(evt);
+            }
+        });
 
         javax.swing.GroupLayout textStatusPanelLayout = new javax.swing.GroupLayout(textStatusPanel);
         textStatusPanel.setLayout(textStatusPanelLayout);
         textStatusPanelLayout.setHorizontalGroup(
             textStatusPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(textStatusPanelLayout.createSequentialGroup()
-                .addComponent(jLabel1)
-                .addGap(0, 513, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, textStatusPanelLayout.createSequentialGroup()
+                .addGap(0, 354, Short.MAX_VALUE)
+                .addComponent(encodingLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 148, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(positionLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 168, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(editationModeLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE))
         );
         textStatusPanelLayout.setVerticalGroup(
             textStatusPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jLabel1)
+            .addComponent(editationModeLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(positionLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+            .addComponent(encodingLabel, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
 
         statusBarPanel.add(textStatusPanel, java.awt.BorderLayout.CENTER);
@@ -479,7 +562,7 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         editCopyMenuItem.setText("Copy");
         editMenu.add(editCopyMenuItem);
 
-        editPasteMenuItem.setAction(copyEditAction);
+        editPasteMenuItem.setAction(pasteEditAction);
         editPasteMenuItem.setText("Paste");
         editMenu.add(editPasteMenuItem);
 
@@ -493,11 +576,56 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
 
         mainMenuBar.add(editMenu);
 
+        aboutMenu.setText("About");
+
+        aboutMenuItem.setText("About...");
+        aboutMenuItem.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                aboutMenuItemActionPerformed(evt);
+            }
+        });
+        aboutMenu.add(aboutMenuItem);
+
+        mainMenuBar.add(aboutMenu);
+
         setJMenuBar(mainMenuBar);
 
-        setSize(new java.awt.Dimension(574, 403));
+        setSize(new java.awt.Dimension(715, 481));
         setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    private void aboutMenuItemActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_aboutMenuItemActionPerformed
+        JOptionPane.showMessageDialog(this,
+                "Delta Hexadecimal Editor - Basic Editor\nhttp://deltahex.exbin.org",
+                "About application",
+                JOptionPane.PLAIN_MESSAGE);
+    }//GEN-LAST:event_aboutMenuItemActionPerformed
+
+    private void editationModeLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_editationModeLabelMouseClicked
+        if (evt.getButton() == MouseEvent.BUTTON1) {
+            EditationMode editationMode;
+            if (codeArea.getEditationMode() == EditationMode.INSERT) {
+                editationMode = EditationMode.OVERWRITE;
+            } else {
+                editationMode = EditationMode.INSERT;
+            }
+            codeArea.setEditationMode(editationMode);
+        }
+    }//GEN-LAST:event_editationModeLabelMouseClicked
+
+    private void encodingLabelMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_encodingLabelMouseClicked
+        EncodingSelectionDialog dialog = new EncodingSelectionDialog(this, true);
+        dialog.setEncoding(codeArea.getCharset().name());
+        dialog.setVisible(true);
+        if (dialog.getReturnStatus() == EncodingSelectionDialog.RET_OK) {
+            String encoding = dialog.getEncoding();
+            if (encoding != null) {
+                codeArea.setCharset(Charset.forName(encoding));
+                codeArea.repaint();
+                encodingLabel.setText(encoding);
+            }
+        }
+    }//GEN-LAST:event_encodingLabelMouseClicked
 
     private void newFileActionPerformed() {
         if (!releaseFile()) {
@@ -505,7 +633,12 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         }
 
         ((EditableBinaryData) codeArea.getData()).clear();
+        codeArea.notifyDataChanged();
+        codeArea.repaint();
         undoHandler.clear();
+        updateUndoState();
+        updateClipboardState();
+        file = null;
     }
 
     private void openFileActionPerformed() {
@@ -516,6 +649,11 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
                 file = fileChooser.getSelectedFile();
                 try (FileInputStream stream = new FileInputStream(file)) {
                     ((EditableBinaryData) codeArea.getData()).loadFromStream(stream);
+                    codeArea.notifyDataChanged();
+                    codeArea.repaint();
+                    undoHandler.clear();
+                    updateUndoState();
+                    updateClipboardState();
                 } catch (FileNotFoundException ex) {
                     Logger.getLogger(DeltaHexEditorBasic.class.getName()).log(Level.SEVERE, null, ex);
                 } catch (IOException ex) {
@@ -538,8 +676,39 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         int chooserResult = fileChooser.showSaveDialog(this);
         if (chooserResult == JFileChooser.APPROVE_OPTION) {
             file = fileChooser.getSelectedFile();
+            if (file.exists()) {
+                if (!overwriteFile()) {
+                    return;
+                }
+            }
+
             saveToFile();
         }
+    }
+
+    /**
+     * Asks whether it's allowed to overwrite file.
+     *
+     * @return true if allowed
+     */
+    private boolean overwriteFile() {
+        Object[] options = {"Overwrite", "Cancel"};
+
+        int result = JOptionPane.showOptionDialog(
+                this,
+                "This file aready exists! Do you wish to overwrite it?",
+                "Overwrite File?",
+                JOptionPane.YES_NO_OPTION,
+                JOptionPane.QUESTION_MESSAGE,
+                null, options, options[0]);
+        if (result == JOptionPane.YES_OPTION) {
+            return true;
+        }
+        if (result == JOptionPane.NO_OPTION || result == JOptionPane.CLOSED_OPTION) {
+            return false;
+        }
+
+        return false;
     }
 
     private void exitActionPerformed() {
@@ -583,6 +752,18 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
         return true;
     }
 
+    private void updateUndoState() {
+        undoEditAction.setEnabled(undoHandler.canUndo());
+        redoEditAction.setEnabled(undoHandler.canRedo());
+    }
+
+    private void updateClipboardState() {
+        cutEditAction.setEnabled(codeArea.hasSelection());
+        copyEditAction.setEnabled(codeArea.hasSelection());
+        deleteEditAction.setEnabled(codeArea.hasSelection());
+        pasteEditAction.setEnabled(codeArea.canPaste());
+    }
+
     /**
      * @param args the command line arguments
      */
@@ -620,6 +801,8 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenu aboutMenu;
+    private javax.swing.JMenuItem aboutMenuItem;
     private javax.swing.JButton copyEditButton;
     private javax.swing.JButton cutEditButton;
     private javax.swing.JButton deleteEditButton;
@@ -636,9 +819,10 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
     private javax.swing.JMenuItem editRedoPopupMenuItem;
     private javax.swing.JMenuItem editUndoMenuItem;
     private javax.swing.JMenuItem editUndoPopupMenuItem;
+    private javax.swing.JLabel editationModeLabel;
+    private javax.swing.JLabel encodingLabel;
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JPopupMenu.Separator jSeparator1;
     private javax.swing.JPopupMenu.Separator jSeparator2;
     private javax.swing.JToolBar.Separator jSeparator3;
@@ -652,6 +836,7 @@ public class DeltaHexEditorBasic extends javax.swing.JFrame {
     private javax.swing.JButton openFileButton;
     private javax.swing.JMenuItem openFileMenuItem;
     private javax.swing.JButton pasteEditButton;
+    private javax.swing.JLabel positionLabel;
     private javax.swing.JButton redoEditButton;
     private javax.swing.JMenuItem saveAsFileMenuItem;
     private javax.swing.JButton saveFileButton;
